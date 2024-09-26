d430f8f08a86fb4b43eace9f1b0ccb1ad37409ce
MagNording <magnus_nording@msn.com>
2024-09-26 16:17:07 +0200

    Using an array for the recipes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,150 +1,92 @@
-import { useState } from 'react'
-import '../styles/App.css'
+import { useState } from "react";
+import "../styles/App.css";
 
 function App() {
-  return (
-    <div className='body'>
-      <header className='header-top'>
-        <h1 className='header-title'>Dessert Heaven</h1>
-        <h2 className='header-citation'>"A Taste of Heaven in Every Bite"</h2>
-        <h2 className='header-citation'>"Where Every Dessert Tells a Story"</h2>
-        <span className='arrow'>&#x25BC;</span>
-      </header>
-      <nav className='nav'>
-        <hr />
-        <ul className='nav-list'>
-          <li><button className='category'>Category</button></li>
-          <li><button className='category'>Category</button></li>
-          <li><button className='category'>Category</button></li>
+  const recipes = [
+    {
+      name: "Recipe 1",
+      description: "Description of Recipe 1",
+      img: "/brooke-lark-pGM4sjt_BdQ-unsplash.jpg",
+    },
 
-          <li><button className='category'>Category</button></li>
-          <li><button className='category'>Category</button></li>
-          <li><button className='category'>Category</button></li>
+    {
+      name: "Recipe 2",
+      description: "Description of Recipe 2",
+      img: "/brooke-lark-pGM4sjt_BdQ-unsplash.jpg",
+    },
+
+    {
+      name: "Recipe 3",
+      description: "Description of Recipe 3",
+      img: "/brooke-lark-pGM4sjt_BdQ-unsplash.jpg",
+    },
+
+    {
+      name: "Recipe 4",
+      description: "Description of Recipe 4",
+      img: "/brooke-lark-pGM4sjt_BdQ-unsplash.jpg",
+    },
+
+    {
+      name: "Recipe 5",
+      description: "Description of Recipe 5",
+      img: "/brooke-lark-pGM4sjt_BdQ-unsplash.jpg",
+    },
+  ];
+
+  return (
+    <div className="body">
+      <header className="header-top">
+        <h1 className="header-title">Dessert Heaven</h1>
+        <h2 className="header-citation">"A Taste of Heaven in Every Bite"</h2>
+        <h2 className="header-citation">"Where Every Dessert Tells a Story"</h2>
+        <span className="arrow">&#x25BC;</span>
+      </header>
+
+      <nav className="nav">
+        <hr />
+        <ul className="nav-list">
+          <li>
+            <button className="category">Category 1</button>
+          </li>
+          <li>
+            <button className="category">Category 2</button>
+          </li>
+          <li>
+            <button className="category">Category 3</button>
+          </li>
         </ul>
         <hr />
       </nav>
 
-
-      <div className='search-div'>
+      <div className="search-div">
         <input type="text" placeholder="Search..." />
-        <button type="submit" className='search-button'>Search</button>
+        <button type="submit" className="search-button">
+          Search
+        </button>
       </div>
 
-
-
-      <main className='card-container'>
-
-        <section className='card'>
-          <h2>Namn</h2>
-          <img src="../public/brooke-lark-pGM4sjt_BdQ-unsplash.jpg" alt="Recipe 1" />
-          <ul className='recipe-list'>
-            <li>
-              <h3>Recipe 1</h3>
-              <p>Description of Recipe 1</p>
-              <button>View Recipe</button>
-            </li>
-          </ul>
-        </section>
-
-        <section className='card'>
-          <h2>Namn</h2>
-          <img src="../public/brooke-lark-pGM4sjt_BdQ-unsplash.jpg" alt="Recipe 2" />
-          <ul className='recipe-list'>
-            <li>
-              <h3>Recipe 2</h3>
-              <p>Description of Recipe 2</p>
-              <button>View Recipe</button>
-            </li>
-          </ul>
-        </section>
-
-        <section className='card'>
-          <h2>Namn</h2>
-          <ul className='recipe-list'>
-            <li>
-              <img src="../public/brooke-lark-pGM4sjt_BdQ-unsplash.jpg" alt="Recipe 3" />
-              <h3>Recipe 3</h3>
-              <p>Description of Recipe 3</p>
-              <button>View Recipe</button>
-            </li>
-          </ul>
-        </section>
-
-
-        <section className='card'>
-          <h2>Namn</h2>
-          <ul className='recipe-list'>
-            <li>
-              <img src="../public/brooke-lark-pGM4sjt_BdQ-unsplash.jpg" alt="Recipe 3" />
-              <h3>Recipe 3</h3>
-              <p>Description of Recipe 3</p>
-              <button>View Recipe</button>
-            </li>
-          </ul>
-        </section>
-
-
-        <section className='card'>
-          <h2>Namn</h2>
-          <ul className='recipe-list'>
-            <li>
-              <img src="../public/brooke-lark-pGM4sjt_BdQ-unsplash.jpg" alt="Recipe 3" />
-              <h3>Recipe 3</h3>
-              <p>Description of Recipe 3</p>
-              <button>View Recipe</button>
-            </li>
-          </ul>
-        </section>
-
-
-        <section className='card'>
-          <h2>Namn</h2>
-          <ul className='recipe-list'>
-            <li>
-              <img src="../public/brooke-lark-pGM4sjt_BdQ-unsplash.jpg" alt="Recipe 3" />
-              <h3>Recipe 3</h3>
-              <p>Description of Recipe 3</p>
-              <button>View Recipe</button>
-            </li>
-          </ul>
-        </section>
-
-
-        <section className='card'>
-          <h2>Namn</h2>
-          <ul className='recipe-list'>
-            <li>
-              <img src="../public/brooke-lark-pGM4sjt_BdQ-unsplash.jpg" alt="Recipe 3" />
-              <h3>Recipe 3</h3>
-              <p>Description of Recipe 3</p>
-              <button>View Recipe</button>
-            </li>
-          </ul>
-        </section>
-
-
-        <section className='card'>
-          <h2>Namn</h2>
-          <ul className='recipe-list'>
-            <li>
-              <img src="../public/brooke-lark-pGM4sjt_BdQ-unsplash.jpg" alt="Recipe 3" />
-              <h3>Recipe 3</h3>
-              <p>Description of Recipe 3</p>
-              <button>View Recipe</button>
-            </li>
-          </ul>
-        </section>
+      <main className="card-container">
+        {recipes.map((recipe, index) => (
+          <section className="card" key={index}>
+            <h2>{recipe.name}</h2>
+            <img src={recipe.img} alt={recipe.name} />
+            <ul className="recipe-list">
+              <li>
+                <h3>{recipe.name}</h3>
+                <p>{recipe.description}</p>
+                <button>View Recipe</button>
+              </li>
+            </ul>
+          </section>
+        ))}
       </main>
 
-      <footer className='footer'>
+      <footer className="footer">
         <p>Recipe Website &copy; 2022 GRUPP 3, JU23 & ITP24</p>
-
       </footer>
-
     </div>
-
-
-  )
+  );
 }
 
 export default App;
