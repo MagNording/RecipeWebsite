dd6c1875d96ae30fef2a76832b7fb3a98f3e8750
EmSley77 <emanuelsleyman@gmail.com>
2024-09-26 13:34:25 +0200

    adding arrow in header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,8 +6,9 @@ function App() {
     <div className='body'>
       <header className='header-top'>
         <h1 className='header-title'>Dessert Heaven</h1>
-        <h2>"A Taste of Heaven in Every Bite"</h2>
-        <h2>"Where Every Dessert Tells a Story"</h2>
+        <h2 className='header-citation'>"A Taste of Heaven in Every Bite"</h2>
+        <h2 className='header-citation'>"Where Every Dessert Tells a Story"</h2>
+        <span className='arrow'>&#x25BC;</span>
       </header>
       <nav className='nav'>
         <hr />
