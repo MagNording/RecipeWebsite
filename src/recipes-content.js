2ae6b782f91b2e66842276265fa2f2d6334d8be4
Depinder Kaur <depinder.kaur@iths.se>
2024-09-26 16:43:41 +0200

    Created a new .js file to have test data for recipes.

---
+++ src/recipes-content.js
@@ -0,0 +1,29 @@
+const recipeContent = [
+    {
+        id: 1,
+        name: "Recipe 1",
+        image: "../src/images/cupcakes.jpg",
+        description: "text 1"
+    },
+    {
+        id: 2,
+        name: "Recipe 2",
+        image: "../src/images/pic1.jpg",
+        description: "desc 2"
+    },
+    {
+        id: 3,
+        name: "Recipe 3",
+        image: "../src/images/macaroons.jpg",
+        description: "desc 3"
+    },
+    {
+        id: 4,
+        name: "Recipe 4",
+        image: "../src/images/pic2.jpg",
+        description: "desc 4"
+    }
+  ];
+  
+  export default recipeContent;
+  
