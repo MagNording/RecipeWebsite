92661bdbd1338892074b128e9358e07c19fc3a2f
EmSley77 <emanuelsleyman@gmail.com>
2024-10-01 14:48:08 +0200

    creating handle submit for comment

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,31 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CommentForm.css';
 
 function CommentForm() {
 
+    const [message, setMessage] = useState('')
+
+    const [firstName, setFirstName] = useState('')
+    const [anonymous, setAnonymous] = useState(false)
+    const [comment, setComment] = useState('')
+
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const displayName = anonymous ? 'Anonymous' : firstName
+        console.log(`name ${displayName}, comment ${comment}`)
+        setMessage('tack för din kommentar!')
+
+
+        setFirstName('');
+        setComment('');
+        setAnonymous(false);
+    }
+
     return (
         <div className='main'>
             <div className='container'>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h2>Kommentar</h2>
                     <div className='name-div'>
                         <label htmlFor="name"></label>
-                        <input type="text" id="name" placeholder='ange namn' required /> <br />
+                        <input type="text"
+                            id="name"
+                            placeholder='ange namn'
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
+                            disabled={anonymous}
+                            required={!anonymous} /> <br />
 
                         <label htmlFor="anonymous" className='anonymous-label'>förbli anonym?</label>
-                        <input type="checkbox"id="anonymous" /> 
+                        <input type="checkbox"
+                            id="anonymous"
+                            checked={anonymous}
+                            onChange={(e) => setAnonymous(e.target.checked)} />
                     </div>
 
                     <div className='comment-div'>
                         <label htmlFor="comment"></label>
-                        <textarea id="comment" placeholder='ange din kommentar' cols={20} rows={5} />
+                        <textarea id="comment" placeholder='ange din kommentar' cols={20} rows={5} value={comment} onChange={(e) => setComment(e.target.value)} />
                     </div>
 
                     <div className='button-container'>
                         <button type="submit">Ange kommentar</button>
                         <button type='submit'>Tillbaka</button>
                     </div>
+
                 </form>
+                <br />
+                <div>
+                    {message && <p>{message}</p>}
+                </div>
+
             </div>
         </div>
     )
