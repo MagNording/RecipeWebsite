e9490130f1b087768aed6c8fc9fded815d4b8255
Depinder Kaur <depinder.kaur@iths.se>
2024-10-01 19:01:41 +0200

    Created a 'pages' folder containing different pages to be displayed with the help of React-Router. Moved the code from the App.jsx to Home.jsx to apply Routing effectively.

---
+++ src/pages/Error.jsx
@@ -0,0 +1,7 @@
+export default function ErrorPage() {
+    return (
+        <div className='error-box'>
+            <strong>Error:</strong> Could not find this page. Please try again.
+        </div>
+    );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import HomeHeader from "../components/Header/HomeHeader";
+import NavBar from "../components/NavBar/NavBar";
+import SearchBar from "../components/NavBar/SearchBar";
+import Main from "../components/Main/Main";
+import Footer from "../components/Footer/Footer";
+
+export default function HomePage() {
+    return (
+        <div className='body'>
+            <HomeHeader />
+            <NavBar />
+            <SearchBar />
+            <Main />
+            <Footer />
+        </div>
+    );
+}
