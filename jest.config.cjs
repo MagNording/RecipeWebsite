b78b4c20d78fd419a980b09256839caea2079de0
Depinder Kaur <depinder.kaur@iths.se>
2024-10-08 21:47:20 +0200

    Slight modifcation in the jest-config file.

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   },
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/mocks/fileMock.js',
   },
   extensionsToTreatAsEsm: ['.jsx'], // Ensure Jest treats these as ESM
 };
