876319730fc7c64694a1de9be36b5c4f7383b2ea
Depinder Kaur <depinder.kaur@iths.se>
2024-10-11 10:08:51 +0200

    Re-factorised ViewRecipe file, by moving fetch-calls to the Utils file in the components folder.

--- src/components/Utils.jsx
+++ src/components/Utils.jsx
@@ -1,4 +1,4 @@
-const fetchRecipes = async () => {
+export const fetchRecipes = async () => {
     try {
         const response = await fetch('https://recept3-bolen.reky.se/recipes');
         const data = await response.json();
@@ -10,7 +10,7 @@ const fetchRecipes = async () => {
 }
 
 // get all available categories from database
-function getAllCategories(data) {
+export function getAllCategories(data) {
     const categoriesArray =  data.map(recipe => recipe.categories) ;
     let mergedCategories = [];
     
@@ -29,7 +29,7 @@ function getAllCategories(data) {
     return availableCategories;
 }
 
-const fetchRecipesByCategory = async (selectedCategory) => {
+export const fetchRecipesByCategory = async (selectedCategory) => {
     try {
         const response = await fetch('https://recept3-bolen.reky.se/categories/' + selectedCategory + '/recipes');
         const data = await response.json();
@@ -40,4 +40,62 @@ const fetchRecipesByCategory = async (selectedCategory) => {
     }
 }
 
-export { fetchRecipes, getAllCategories, fetchRecipesByCategory };
+const unitConversionMap = {
+    "gram": "gm",
+    "deciliter": "dl",
+    "teskedar": "tsk",
+    "tesked": "tsk",
+    "matsked": "msk",
+    "matskedar": "msk",
+    "stycken": "st"
+};
+
+export const getShortUnit = (unit) => {
+    return unitConversionMap[unit] || unit;             // Default to original unit if not in map
+};
+
+export const fetchRecipeById = async (recipeId) => {
+    try {
+        const response = await fetch(`https://recept3-bolen.reky.se/recipes/${recipeId}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(`Error fetching recipe with id: ${recipeId}`, error);
+        return null;
+    }
+}
+
+export const fetchCommentsByRecipeId = async (recipeId) => {
+    try {
+        const response = await fetch(`https://recept3-bolen.reky.se/recipes/${recipeId}/comments`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(`Error fetching comments for recipe with id: ${recipeId}`, error);
+        return [];
+    }
+}
+
+// Här ska en fetch-request göra till en API som sparar ratingen
+export const saveRecipeRating = async (recipeId, recipeRating) => {
+    try {
+        const response = await fetch(`https://recept3-bolen.reky.se/recipes/${recipeId}/ratings`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ rating: recipeRating }),
+        });
+
+        if (response.ok) {
+            return true;
+        } else {
+            console.error('Error saving rating:', response.statusText);
+            return false;
+        }
+
+    } catch (error) {
+        console.error('Error saving rating:', error);
+        return false;
+    }
+}
