636b01c38657569e8cff484469c5a1940a252f2b
Depinder Kaur <depinder.kaur@iths.se>
2024-10-18 15:32:07 +0200

    fixed responsiveness of the recipe-cards on the home-page

--- src/components/Main/RecipeCard.jsx
+++ src/components/Main/RecipeCard.jsx
@@ -13,24 +13,32 @@ export default function RecipeCard(props) {
   return (
     <Link to={`/recipes/${props.id}`} className={style['card-link']}>
       <section className={style.card}>
-        <img src={props.imageUrl} alt={props.title} />
-        <ul className={style['recipe-list']}>
-          <li key={props.id}>
-            <h3>{props.title}</h3>
-            <p className={style['desc']}>{props.description}</p>
-            <div className={style['stars']}>
-              {displayStarRating(props.rating)}
-            </div>
-            <ul className={style.details}>
-              <li className={style.time}>  <FontAwesomeIcon icon={faClock} />
-                <span> {props.time} min</span>
-              </li>
-              <li>
-                {renderDifficultyBars(props.time, style)}
-              </li>
-            </ul>
-          </li>
-        </ul>
+        <div>
+          <img src={props.imageUrl} alt={props.title} />
+          <ul className={style['recipe-list']}>
+            <li key={props.id}>
+              <h3>{props.title}</h3>
+              <p className={style['desc']}>{props.description}</p>
+              <div className={style['stars']}>
+                {displayStarRating(props.rating)}
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ul className={style['recipe-list']}>
+            <li key={props.id}>
+              <ul className={style.details}>
+                <li className={style.time}>  <FontAwesomeIcon icon={faClock} />
+                  <span> {props.time} min</span>
+                </li>
+                <li>
+                  {renderDifficultyBars(props.time, style)}
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>        
       </section>
     </Link>
   );
